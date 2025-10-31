e842f2f53d1f9086e5293a7ed7b16f0447827bca
CallMeRevy <108072610+NatureKiddo@users.noreply.github.com>
2025-11-01 00:07:44 +0200

    Add files via upload

---
+++ ASP.NET/Event-Planner/Stuff with the project part 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Event_Manager.Startup))]
+namespace Event_Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
